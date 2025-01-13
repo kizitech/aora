4489da80d8a2776639caf8bfb25fa0045085586d
Ohani Kizito <kizitoohani@gmail.com>
2025-01-13 18:53:32 +0100

    added the auth pages and the index page

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -40,12 +40,12 @@ const RootLayout = () => {
 
   return (
     // <GlobalProvider>
-      <Stack>
-        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} /> */}
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        {/* <Stack.Screen name="search/[query]" options={{ headerShown: false }} /> */}
-      </Stack>
+    <Stack>
+      {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      {/* <Stack.Screen name="search/[query]" options={{ headerShown: false }} /> */}
+    </Stack>
     // </GlobalProvider>
   );
 };
