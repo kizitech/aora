201982580b8412ab352f8cc08804427f1c2e7194
Ohani Kizito <kizitoohani@gmail.com>
2025-01-20 18:56:32 +0100

    recent working

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,7 +1,7 @@
 import { Redirect, Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 
-import { Loader } from "../../components";
+// import { Loader } from "../../components";
 // import { useGlobalContext } from "../../context/GlobalProvider";
 
 const AuthLayout = () => {
--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,15 @@
 import { StatusBar } from "expo-status-bar";
-import { Redirect, router } from "expo-router";
+import { Link, Redirect, router } from "expo-router";
 import { View, Text, Image, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { images } from "../constants";
 import { CustomButton } from "../components";
-// import { useGlobalContext } from "../context/GlobalProvider";
 
 const Welcome = () => {
-  // const { loading, isLogged } = useGlobalContext();
-
-  // if (!loading && isLogged) return <Redirect href="/home" />;
 
   return (
     <SafeAreaView className="bg-primary h-full">
-      {/* <Loader isLoading={loading} /> */}
 
       <ScrollView
         contentContainerStyle={{
@@ -58,6 +53,7 @@ const Welcome = () => {
             handlePress={() => router.push("/sign-in")}
             containerStyles="w-full mt-7"
           />
+          <Link href="/home" className="text-3xl text-green-400">Go to Home</Link>
         </View>
       </ScrollView>
 
